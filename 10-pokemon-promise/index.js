5680f44435d092ee279b0cda7d4a1917a064c902
ViacheslavSave <nikolya-ivanov-9191@mail.ru>
2023-11-16 19:32:22 +0300

    Изменения после код ревью

--- 10-pokemon-promise/index.js
+++ 10-pokemon-promise/index.js
@@ -1,8 +1,18 @@
 "use strict";
-fetch("https://pokeapi.co/api/v2/pokemon/ditto/")
-	.then((response) => response.json())
+ fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+	.then((response) => {
+		if (response.ok) {
+			return response.json();
+		}
+		throw new Error("Status code: " + response.status);
+	})
 	.then(({ abilities }) => fetch(abilities[0].ability.url))
-	.then((response) => response.json())
+	.then((response) => {
+		if (response.ok) {
+			return response.json();
+		}
+		throw new Error("Status code: " + response.status);
+	})
 	.then(({ effect_entries }) => effect_entries.find((ef) => ef.language.name === "en").effect)
 	.then((str) => console.log(str))
-	.catch((err) => console.log(err));
+	.catch((err) => console.dir(err));
--- 11-geolocation/index.js
+++ 11-geolocation/index.js
@@ -1,9 +1,12 @@
-"use strict";
-new Promise((resolve, reject) => {
-	navigator.geolocation.getCurrentPosition(
-		({ coords: { longitude, latitude } }) => resolve({ longitude, latitude }),
-		() => reject(new Error("Не удалось получить местоположение"))
-	);
-})
-	.then((position) => console.log(position))
-	.catch((err) => console.log(err));
+if ("geolocation" in navigator) {
+	new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(
+			({ coords: { longitude, latitude } }) => resolve({ longitude, latitude }),
+			() => reject(new Error("Prohibited by user"))
+		);
+	})
+		.then((position) => console.log(position))
+		.catch((err) => console.error(err));
+} else {
+	console.error(new Error("No geolocation object"));
+}
